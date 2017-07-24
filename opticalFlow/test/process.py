1c0cbdff2c618297c9ecd38d705976647960335b
megamusz <megamusz@nvidia.com>
2017-07-24 13:18:02 +0800

    add code for separating predict and final OF result.

--- opticalFlow/test/process.py
+++ opticalFlow/test/process.py
@@ -3,7 +3,7 @@ import os
 def del_files(path):
     for root , dirs, files in os.walk(path):
         for name in files:
-            if name.endswith(".png"):
+            if name.endswith((".png", ".yuv")):
                 os.remove(os.path.join(root, name))
                 
 bin = r'..\build\Release\optflow.exe '
@@ -18,5 +18,9 @@ for i in range(0, 194):
     cmd = '{0} {1} {2}'.format(bin, dir, i)
     print(cmd)
     os.system(cmd)
+    p = '{:06d}_10.png'.format(i)
+    p_c = '{:06d}_10_c.png'.format(i)
+    os.rename(p, 'pred/{0}'.format(p))
+    os.rename(p_c, 'pred_correct/{0}'.format(p))
     
 
